4f42163788bcd264962abbd772908bd22d24510c
megatron-hu <76518032+megatron-hu@users.noreply.github.com>
2023-09-29 18:00:09 +0530

    Delete CODSOFT-master directory

--- CODSOFT-master/app/src/main/java/com/codingstuff/todolist/OnDialogCloseListner.java
+++
@@ -1,8 +0,0 @@
-package com.codingstuff.todolist;
-
-import android.content.DialogInterface;
-
-public interface OnDialogCloseListner {
-
-    void onDialogClose(DialogInterface dialogInterface);
-}
